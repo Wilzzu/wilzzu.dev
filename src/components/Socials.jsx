3db838609230b735a2730dfcff9d88b71cf46937
Wilzzu <wilzzudev@gmail.com>
2024-01-01 18:56:44 +0200

    Change structure, add social icons and links

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,25 +1,52 @@
 import { motion } from "framer-motion";
+import { FiGithub, FiTwitter, FiYoutube } from "react-icons/fi";
+import { FaDiscord } from "react-icons/fa";
 
 const socials = [
 	{
 		name: "GitHub",
-		link: "",
-		image: "",
+		link: "https://github.com/Wilzzu",
+		image: (
+			<FiGithub
+				size={48}
+				strokeWidth={1.4}
+				className="opacity-40 group-hover:opacity-80 duration-500"
+			/>
+		),
 	},
 	{
 		name: "Twitter",
-		link: "",
-		image: "",
+		link: "https://twitter.com/Wilzzu",
+		image: (
+			<FiTwitter
+				size={48}
+				strokeWidth={1.4}
+				className="opacity-40 group-hover:opacity-80 duration-500"
+			/>
+		),
 	},
 	{
 		name: "YouTube",
-		link: "",
-		image: "",
+		link: "https://www.youtube.com/@thewilzzu",
+		image: (
+			<FiYoutube
+				size={48}
+				strokeWidth={1.4}
+				className="opacity-40 group-hover:opacity-80 duration-500"
+			/>
+		),
 	},
 	{
 		name: "Discord",
 		link: "",
-		image: "",
+		image: (
+			<FaDiscord
+				size={48}
+				fill="transparent"
+				strokeWidth={36}
+				className="opacity-40 group-hover:opacity-80 duration-500"
+			/>
+		),
 	},
 ];
 
@@ -38,9 +65,11 @@ const item = {
 		y: 0,
 		opacity: 1,
 		transition: {
-			type: "tween",
-			duration: 0.35,
-			ease: "easeOut",
+			y: { type: "tween", duration: 1, ease: "easeOut" },
+			opacity: {
+				duration: 2,
+				ease: "easeInOut",
+			},
 		},
 	},
 	hidden: {
@@ -55,14 +84,16 @@ const Socials = () => {
 			initial="hidden"
 			animate="start"
 			variants={container}
-			className="absolute bottom-0 h-14 w-full flex bg-neutral-800 justify-between">
+			className="h-14 w-full flex justify-between">
 			{/* Icons */}
 			{socials.map((social) => (
 				<motion.li
 					key={social.name}
-					className="w-14 h-14 flex items-center justify-center"
+					className="group w-14 h-14 flex items-center justify-center"
 					variants={item}>
-					<img src={social.image} alt={social.name} className="w-8 h-8" />
+					<a target="_blank" rel="noopener noreferrer" href={social.link}>
+						{social.image}
+					</a>
 				</motion.li>
 			))}
 		</motion.ul>
--- src/components/start/Logo.jsx
+++ src/components/start/Logo.jsx
@@ -36,38 +36,47 @@ const Logo = (props) => {
 	}, [controls]);
 
 	return (
-		// Animated moving wrapper for the logo
+		// Full height container that moves to the left after logo reveal
 		<motion.div
 			layout
 			id="logoContainer"
-			variants={variants}
-			initial="hidden"
-			animate={controls}
 			transition={{ layout: { duration: 2, type: "tween", ease: [0.61, 0, 0.59, 0.93] } }} //TODO: Make a better animation
-			onAnimationComplete={() => props.setInitialLogoAnimationComplete(true)}
 			onLayoutAnimationComplete={handleLayoutAnimationComplete}
-			className="relative group w-[40%] flex flex-col items-center drop-shadow-2xl select-none p-10 z-10">
-			{/* Logo with tilt */}
-			{/* TODO: Add hovering effect and tilting */}
-			<Tilt
-				className="bg-[url('./src/assets/LogoBG.png')] bg-cover bg-center bg-no-repeat flex justify-center items-center w-full h-full aspect-square rounded-[2rem] transform-style-3d"
-				perspective={800}
-				glareEnable={true}
-				glareMaxOpacity={0.2}
-				glareColor="#ffffff"
-				glarePosition="bottom"
-				glareBorderRadius="2rem"
-				transitionSpeed={2000}
-				gyroscope={true}>
-				{/* Inner logo letter */}
-				<img
-					className="inner-element w-1/4 transform-style-3d translate-z-4 drop-shadow-2xl group-hover:scale-125 duration-500 ease-out"
-					src="./src/assets/LogoLetter.png"
-					alt="W letter logo"
-					draggable="false"
-				/>
-			</Tilt>
-			{firstLayoutAnimationComplete && <Socials />}
+			className="relative w-[40%] h-full flex flex-col items-center justify-center drop-shadow-2xl select-none z-10">
+			{/* Container for logo and socials */}
+			<motion.div layout className="w-full h-full flex flex-col items-center justify-center gap-8">
+				{/* Container for logo, to move it upwards at start */}
+				<motion.div
+					layout
+					variants={variants}
+					initial="hidden"
+					animate="start"
+					onAnimationComplete={() => props.setInitialLogoAnimationComplete(true)}
+					transition={{ layout: { duration: 1, type: "tween", ease: [0.61, 0, 0.59, 0.93] } }}
+					className="group px-10 w-full">
+					{/* Hoverable logo */}
+					{/* TODO: Add automatic hovering effect and tilting */}
+					<Tilt
+						className="bg-[url('./src/assets/LogoBG.png')] bg-cover bg-center bg-no-repeat flex justify-center items-center aspect-square rounded-[2rem] transform-style-3d"
+						perspective={800}
+						glareEnable={true}
+						glareMaxOpacity={0.2}
+						glareColor="#ffffff"
+						glarePosition="bottom"
+						glareBorderRadius="2rem"
+						transitionSpeed={2000}
+						gyroscope={true}>
+						{/* Inner logo letter */}
+						<img
+							className="inner-element w-1/4 transform-style-3d translate-z-4 drop-shadow-2xl group-hover:scale-125 duration-500 ease-out"
+							src="./src/assets/LogoLetter.png"
+							alt="W letter logo"
+							draggable="false"
+						/>
+					</Tilt>
+				</motion.div>
+				{firstLayoutAnimationComplete && <Socials />}
+			</motion.div>
 		</motion.div>
 	);
 };
