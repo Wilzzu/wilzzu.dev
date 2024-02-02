c7e3092454f1b6ce3fe8a06d6bbfe1fe829bcf9d
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2024-02-03 00:26:02 +0200

    Add animations to about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,19 +1,46 @@
 import Contact from "./Contact";
+import { motion } from "framer-motion";
+
+// Animation variants
+const container = {
+	visible: {
+		opacity: 1,
+		transition: {
+			staggerChildren: 0.12,
+			opacity: { duration: 1, ease: "easeInOut" },
+		},
+	},
+	hidden: { opacity: 0 },
+	exit: { opacity: 0, transition: { duration: 2, ease: "easeInOut" } },
+};
+
+const item = {
+	visible: { y: 0, opacity: 1, transition: { type: "spring", stiffness: 34, damping: 12 } },
+	hidden: { y: 52, opacity: 0 },
+	exit: { y: 0, opacity: 0, transition: { duration: 0 } },
+};
 
 const About = () => {
 	return (
 		// Card container
-		<div className="flex flex-col h-full max-h-[37rem] bg-gradient-to-tr from-[#19191975] to-[#2A2A2A75]  backdrop-blur-md rounded-2xl gap-3 items-center justify-between py-10 px-20 text-center">
+		<motion.div
+			initial="hidden"
+			animate="visible"
+			exit="exit"
+			variants={container}
+			className="flex flex-col h-full max-h-[37rem] bg-gradient-to-br from-[#19191975] to-[#2A2A2A75] backdrop-blur-md rounded-2xl gap-3 items-center justify-between py-10 px-20 text-center leading-relaxed text-[0.94rem] text-[#d6d6d6]">
 			{/* Title */}
-			<h1 className="text-[1.76rem] font-semibold mt-1 text-neutral-200">{"Hi, I'm Wilzzu!"}</h1>
+			<motion.h1 variants={item} className="text-[1.76rem] font-semibold mt-1 text-neutral-200">
+				{"Hi, I'm Wilzzu!"}
+			</motion.h1>
 
 			{/* Content */}
-			<p className="leading-relaxed text-[0.94rem] text-[#d6d6d6]">
+			<motion.p variants={item}>
 				{
 					"I'm a 24 year old student from Finland, who loves creating fun projects around the things I'm passionate about. With a background in graphic design, I focus on the design and try to make everything look as good as possible."
 				}
-				<br />
-				<br />
+			</motion.p>
+			<motion.p variants={item}>
 				{
 					'I\'m also a very "DYI" type of person, so instead of using pre-made designs or templates, I like to create everything from scratch to suit my preferences.'
 				}
@@ -21,16 +48,17 @@ const About = () => {
 				{
 					"All the projects and designs are completely made by me, no tutorials or templates were used."
 				}
-				<br />
-				<br />
+			</motion.p>
+			<motion.p variants={item}>
 				{
 					"Besides programming, I enjoy playing video games, editing photos and videos, and tinkering with small electronic projects."
 				}
-				<br />
-				<br /> {"Feel free to contact me, I'm always open to new adventures! :)"}
-			</p>
+			</motion.p>
+			<motion.p variants={item}>
+				{"Feel free to contact me, I'm always open to new adventures! :)"}
+			</motion.p>
 			<Contact />
-		</div>
+		</motion.div>
 	);
 };
 
--- src/components/about/Contact.jsx
+++ src/components/about/Contact.jsx
@@ -1,12 +1,28 @@
 import Socials from "../Socials";
 import socials from "../../configs/socials.json";
+import { motion } from "framer-motion";
+
+const container = {
+	visible: { transition: { delayChildren: 0.15 } },
+	hidden: {},
+};
+
+const item = {
+	visible: { y: 0, opacity: 1, transition: { type: "spring", stiffness: 44, damping: 18 } },
+	hidden: { y: 14, opacity: 0 },
+	exit: { y: 0, opacity: 0, transition: { duration: 0 } },
+};
 
 const Contact = () => {
 	return (
-		<div>
-			<Socials socials={socials.contact} style={"justify-center gap-8 scale-90"} direction={20} />
-			<p className="text-sm text-neutral-400">{"Contact me on Twitter or via email."}</p>
-		</div>
+		<motion.div variants={container}>
+			<motion.div variants={item}>
+				<Socials socials={socials.contact} style={"justify-center gap-8 scale-90"} direction={20} />
+			</motion.div>
+			<motion.p variants={item} className="text-sm text-neutral-400">
+				{"Contact me on Twitter or via email."}
+			</motion.p>
+		</motion.div>
 	);
 };
 
