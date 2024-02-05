9f857c7bcf9e1f1cd22e9eb22964d08c31903099
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2024-02-05 05:41:58 +0200

    Add title animation to about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,3 +1,4 @@
+import AnimatedText from "../AnimatedText";
 import Contact from "./Contact";
 import { cubicBezier, motion } from "framer-motion";
 
@@ -18,7 +19,7 @@ const container = {
 	},
 };
 
-const item = {
+const text = {
 	visible: {
 		y: 0,
 		rotate: 0.01, // To make the animation render smoother
@@ -26,6 +27,16 @@ const item = {
 		transition: { duration: 2.4, ease: cubicBezier(0.09, 0.64, 0.26, 1) },
 	},
 	hidden: { y: 60, opacity: 0 },
+	exit: {
+		opacity: 0,
+		transition: {
+			duration: 0.3,
+			ease: "easeIn",
+		},
+	},
+};
+
+const title = {
 	exit: {
 		opacity: 0,
 		transition: {
@@ -45,28 +56,30 @@ const About = () => {
 			variants={container}
 			className="flex items-center justify-center h-full w-full max-h-[37rem] bg-gradient-to-br from-[#19191975] to-[#2A2A2A75] backdrop-blur-md rounded-2xl py-10 px-4">
 			<div className="flex flex-col max-w-[31rem] gap-4 text-justify items-center justify-between leading-relaxed text-sm text-[#d6d6d6]">
-				<motion.h1 variants={item} className="text-[1.76rem] font-semibold mb-3 text-neutral-200">
-					{"Hi, I'm Wilzzu! 👋"}
-				</motion.h1>
 				{/* Title */}
+				<motion.span
+					variants={title}
+					className="text-[1.76rem] font-semibold mb-3 text-neutral-200">
+					<AnimatedText text="Hi, I'm Wilzzu! 👋" delay={0.6} duration={0.4} />
+				</motion.span>
 
 				{/* Content */}
-				<motion.p variants={item}>
+				<motion.p variants={text}>
 					{
 						"I'm a 24 year old student from Finland, who enjoys creating fun projects around my passions. With a background in graphic design, I focus on the design and try to make everything look as good as possible."
 					}
 				</motion.p>
-				<motion.p variants={item}>
+				<motion.p variants={text}>
 					{
 						'I\'m a "Do-It-Yourself" type of person, so instead of using pre-made designs or templates, I like to create everything from scratch to suit my preferences. All the projects and designs are completely made by me, no tutorials or templates were used.'
 					}
 				</motion.p>
-				<motion.p variants={item}>
+				<motion.p variants={text}>
 					{
 						"Besides programming, I enjoy playing video games, editing photos and videos, and tinkering with small electronic projects in my free time."
 					}
 				</motion.p>
-				<motion.p className="mt-6" variants={item}>
+				<motion.p className="mt-6" variants={text}>
 					{"Feel free to contact me, I'm always open to new adventures! :)"}
 				</motion.p>
 				<Contact />
