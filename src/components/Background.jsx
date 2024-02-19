fd2687035c20f7c525f88d9d65d2325f3b5cd6a8
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2024-02-20 00:04:19 +0200

    Move mouse tracking to its own custom hook

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,60 +1,26 @@
-import { motion, useMotionValue, useSpring } from "framer-motion";
-import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
+import useMousePosition from "../hooks/useMousePosition";
 
 // Animation variants
 const variants = {
-	visible: (showGlow) => ({
-		opacity: showGlow ? 1 : 0,
-		scale: showGlow ? 1 : 0,
-		transition: { type: "tween", duration: showGlow ? 1.3 : 0.5, ease: "easeInOut" },
+	visible: (visible) => ({
+		opacity: visible ? 1 : 0,
+		scale: visible ? 1 : 0,
+		transition: { type: "tween", duration: visible ? 1.3 : 0.5, ease: "easeInOut" },
 	}),
 	hidden: { opacity: 0, scale: 0 },
 };
 
-const smoothing = { stiffness: 20, damping: 10 };
-
 const Background = () => {
-	const [showGlow, setShowGlow] = useState(false);
+	const { smoothPosition, visible } = useMousePosition(256);
 
-	const mousePos = {
-		x: useMotionValue(0),
-		y: useMotionValue(0),
-	};
-
-	const smoothPosition = {
-		x: useSpring(mousePos.x, smoothing),
-		y: useSpring(mousePos.y, smoothing),
-	};
-
-	// Track mouse
-	// TODO: Fix erratic movement
-	useEffect(() => {
-		const onMouseMove = (e) => {
-			const { clientX, clientY } = e;
-			// Center div to the mouse position
-			mousePos.x.set(clientX - 256);
-			mousePos.y.set(clientY - 256);
-			setShowGlow(true);
-		};
-
-		const hideGlow = () => {
-			setShowGlow(false);
-		};
-
-		document.addEventListener("mousemove", onMouseMove);
-		document.addEventListener("mouseleave", hideGlow);
-
-		return () => {
-			document.removeEventListener("mousemove", onMouseMove);
-			document.removeEventListener("mouseleave", hideGlow);
-		};
-	}, [mousePos.x, mousePos.y]);
-	// TODO: Add custom cursor
 	return (
 		<motion.div
 			initial={{ opacity: 0 }}
 			animate={{ opacity: 1 }}
 			// TODO: Add a linear sweep initial transition
+			// TODO: Add same color light streaks going down in an angle
 			transition={{ delay: 3, duration: 2, ease: "easeInOut" }}
 			className="w-full h-full fixed overflow-hidden">
 			{/* Background line colors */}
@@ -67,7 +33,7 @@ const Background = () => {
 				}}
 				initial="hidden"
 				animate="visible"
-				custom={showGlow}
+				custom={visible}
 				variants={variants}
 				className="w-[512px] h-[512px] fixed bg-gradient-radial from-[#FFA700] via-transparent to-50% to-transparent -z-[1]">
 				{/* Grain */}
@@ -77,7 +43,6 @@ const Background = () => {
 			</motion.div>
 			{/* Overlay */}
 			{/* TODO: Change this and make the lines smaller maybe */}
-			{/* TODO: Maybe add slow movement */}
 			<div className="w-full h-full bg-background fixed z-0 bg-clip" />
 		</motion.div>
 	);
---
+++ src/hooks/useMousePosition.js
@@ -0,0 +1,45 @@
+import { useMotionValue, useSpring } from "framer-motion";
+import { useEffect, useState } from "react";
+
+const smoothing = { stiffness: 20, damping: 10 };
+
+const useMousePosition = (center = 0) => {
+	const [visible, setVisible] = useState(false);
+
+	const mousePos = {
+		x: useMotionValue(0),
+		y: useMotionValue(0),
+	};
+
+	const smoothPosition = {
+		x: useSpring(mousePos.x, smoothing),
+		y: useSpring(mousePos.y, smoothing),
+	};
+
+	// Track mouse
+	useEffect(() => {
+		const onMouseMove = (e) => {
+			const { clientX, clientY } = e;
+			// Center div to the mouse position
+			mousePos.x.set(clientX - center);
+			mousePos.y.set(clientY - center);
+			setVisible(true);
+		};
+
+		const hideGlow = () => {
+			setVisible(false);
+		};
+
+		document.addEventListener("mousemove", onMouseMove);
+		document.addEventListener("mouseleave", hideGlow);
+
+		return () => {
+			document.removeEventListener("mousemove", onMouseMove);
+			document.removeEventListener("mouseleave", hideGlow);
+		};
+	}, [mousePos.x, mousePos.y, center]);
+
+	return { mousePos, smoothPosition, visible };
+};
+
+export default useMousePosition;
